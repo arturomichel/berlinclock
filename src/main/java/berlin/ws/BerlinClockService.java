b7958896719994826d165a031cf684399eec9d84
arturomichel <arturo.michel@gmail.com>
2014-05-18 23:32:23 +0100

    Adding command line capability.

--- src/main/java/berlin/ws/BerlinClockService.java
+++ src/main/java/berlin/ws/BerlinClockService.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
@@ -25,22 +24,11 @@ public class BerlinClockService {
 	@Consumes("application/json; charset=utf-8")
 	@Produces("application/json; charset=utf-8")
 	public Response getTime(@QueryParam("time") String time){
-		if(time == null)
+		if("".equals(time))
 			time = sdf.format(new Date());
 		BerlinClock berlin = BerlinClockBuilder.build(time);
 		return Response.status(Status.OK).entity(berlin).build();
 		
 	}
 	
-	/*@GET
-	@Path("/getTime")
-	@Consumes("application/json; charset=utf-8")
-	@Produces("application/json; charset=utf-8")
-	public Response getTime(){
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		String time = sdf.format(new Date());
-		BerlinHeuer berlin = BerlinHeuerParser.toBerlinString(time);
-		berlin.setProvided(false);
-		return Response.status(Status.OK).entity(berlin).build();
-	}*/
 }
